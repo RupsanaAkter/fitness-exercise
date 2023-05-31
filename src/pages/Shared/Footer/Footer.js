dc87a336e11626833b2b19659325bd9a36b97d98
R7558 <rupsanaworld1004@gmail.co>
2023-05-31 17:11:40 +0600

    added footer

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,14 @@
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
-
+import Logo from '../../../assets/img/Logo-1.png'
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
-        </div>
+        <Box mt="80px" bgcolor="#FFF3F4">
+        <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+          <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+        </Stack>
+        <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by me</Typography>
+      </Box>
     );
 };
 
