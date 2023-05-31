653042ef3f0cf48b489da7945f78ad37fe68a295
R7558 <rupsanaworld1004@gmail.co>
2023-05-31 19:17:12 +0600

    added exercises

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../Banner/Banner';
 import SearchExercise from '../SearchExercise/SearchExercise';
+import Exercises from '../Exercises/Exercises';
+import { Box } from '@mui/material';
 
 const Home = () => {
+    const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState('all');
     return (
-        <div>
-            <Banner></Banner>
-            <SearchExercise></SearchExercise>
-        </div>
+        <Box>
+      <Banner />
+      <SearchExercise setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
     );
 };
 
